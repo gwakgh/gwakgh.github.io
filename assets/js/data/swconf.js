c38d868b50f1556d88880075e33591a354fb8f3d
gwakgh <gwakggh304@gmail.com>
2025-05-18 08:50:13 +0900

    배포: gh-pages 최신화

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1747456881',resources: [
+    cacheName: 'chirpy-1747525793',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
