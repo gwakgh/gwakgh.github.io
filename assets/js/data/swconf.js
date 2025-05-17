a2d38e20dbec0570ca0faee9614a078a90dbdaee
gwakgh <gwakggh304@gmail.com>
2025-05-17 13:22:40 +0900

    docs: posting test

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1747455335',resources: [
+    cacheName: 'chirpy-1747455687',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
