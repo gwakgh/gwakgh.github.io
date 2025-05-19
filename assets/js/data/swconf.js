4d970162ebb18e4139dda465fcc7d3831e4c3343
gwakgh <gwakggh304@gmail.com>
2025-05-19 21:56:14 +0900

    deploy: update gh-pages

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1747659030',resources: [
+    cacheName: 'chirpy-1747659374',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
